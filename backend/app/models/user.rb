556d2902193f03b9294ce5c9226e0ec6db3e6c1b
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-03-23 04:06:21 +0800

    added validation in username

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable, :confirmable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  validates :username, presence: true, uniqueness: true
 end
