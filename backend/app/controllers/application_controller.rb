24c7758be1cd681de3e24aeed3ae08b0d95fa2c5
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-03-22 22:03:19 +0800

    added username in devise signup

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   respond_to :json
   include ActionController::MimeResponds
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :username ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :username ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :username ])
+  end
 end
