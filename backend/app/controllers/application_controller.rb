163c35ad74a8574c478cbd90959de805edd24a92
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-03-23 10:29:13 +0800

    added active storage validator gem and added image on devise user

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :username ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :username ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :username, :image ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :username, :image ])
     devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :username ])
   end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :image
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable, :confirmable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :image, content_type: %i[png jpg jpeg], size: { less_than: 2.megabytes }
   validates :username, presence: true, uniqueness: true
 end
