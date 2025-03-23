a5833ba225e0e9d956f3473d9c620df8ea30b3ed
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-03-23 10:39:18 +0800

    partially fixed auth#me, will fix the signature issue

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -1,6 +1,14 @@
 class AuthController < ApplicationController
   before_action :authenticate_user!, only: [ :me ]
   def me
-    render json: { user: current_user }
+    include_image
+  end
+
+  def include_image
+    if current_user.image.attached?
+      render json: { user: current_user, image: url_for(current_user.image) }
+    else
+      render json: { user: current_user }
+    end
   end
 end
